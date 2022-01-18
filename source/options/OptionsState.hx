d50d873789f009e7aa04b76a129c3813c03a8c17
jmxamongusmodder <80003646+jmxamongusmodder@users.noreply.github.com>
2022-01-18 14:32:42 -0500

    CUL

--- source/options/OptionsState.hx
+++ source/options/OptionsState.hx
@@ -59,8 +59,8 @@ class OptionsState extends MusicBeatState
 		DiscordClient.changePresence("Options Menu", null);
 		#end
 
-		var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
-		bg.color = 0xFFea71fd;
+		var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('BG'));
+//		bg.color = 0xFFea71fd;
 		bg.setGraphicSize(Std.int(bg.width * 1.1*scaleRatio));
 		bg.updateHitbox();
 		bg.screenCenter();
